1583453fb6b000b1000efaf3bcbfd4963b180e26
jummy <929440925@qq.com>
2018-11-16 23:31:07 +0800

    自己配置logger

--- src/test/java/com/example/javablog/JavablogApplicationTests.java
+++ src/test/java/com/example/javablog/JavablogApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.javablog;
 import com.example.javablog.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -27,9 +29,19 @@ public class JavablogApplicationTests {
        System.out.println(b);
     }
 
+    Logger logger = LoggerFactory.getLogger(getClass());
     @Test
     public void contextLoads() {
-        System.out.println(person);
+
+        /**
+         * 日志的级别，由低到高，可以调整输出的日志级别
+         */
+        logger.trace("这是trace日志");
+        logger.debug("这是debug日志");
+        logger.info("这是info日志");
+        logger.warn("这是warn日志");
+        logger.error("这是error日志");
+//        System.out.println(person);
     }
 
 }
